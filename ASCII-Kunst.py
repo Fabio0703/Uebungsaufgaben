a202443cd6bf2fc384820ca1c447d429403691c9
Fabio0703 <fabio.bostanci@btc-ag.com>
2024-11-07 09:32:32 +0100

    -

---
+++ ASCII-Kunst.py
@@ -0,0 +1,9 @@
+#a) Würfel aus Sternen
+size = int(input("Gib eine Zahl ein, die größer als 0 ist: "))
+for i in range(size):
+    print ('*'*size)
+
+#b) Dreieck aus Sternen
+scale = int(input("Gib eine Zahl ein, die größer als 0 ist: "))
+for i in range(1, scale+1):
+    print ('*'*i)
