f5b0dfeaa6cfcc8ad73e0395aeda519254a88f78
Fabio0703 <fabio.bostanci@btc-ag.com>
2024-11-13 14:33:31 +0100

    Löschen der irrelevanten Datei

--- Sortieren.py
+++
@@ -1,9 +0,0 @@
-meine_liste = [int(element.strip()) for element in input("Gib die jeweiligen Zahlen ein, getrennt durch Kommas: ").split(',')]
-
-x = len(meine_liste)
-for i in range(x):
-    for j in range(0,x-i-1):
-        if meine_liste[j] > meine_liste[j+1]:
-            meine_liste[j], meine_liste[j+1] = meine_liste[j+1], meine_liste[j]
-
-print(meine_liste)
