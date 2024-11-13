93c97bddb8a559ea4bb5b71c65246a402d8414b7
Fabio0703 <fabio.bostanci@btc-ag.com>
2024-11-13 12:55:17 +0100

    Löschen irrelevanter Dateien für den jeweiligen Branch

--- ALLES GROSS.py
+++
@@ -1,3 +0,0 @@
-text = str(input("Bitte String eingeben: "))
-caps = text.upper() #Methode .upper() wird verwendet, um alle Buchstaben in Großbuchstaben umzuwandeln.
-print (caps)
--- Immer Positiv.py
+++
@@ -1,3 +0,0 @@
-zahl = int(input("Welche Zahl?: "))
-positiv = abs(zahl)
-print(f"Die positive Zahl von {zahl} lautet: {positiv}")
